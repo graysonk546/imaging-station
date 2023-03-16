3c3ae09033e9c078935122acbd4251c5e8d68312
Screw Sorter <kenneth.xing@alumni.ubc.ca>
2023-03-16 11:25:48 -0700

    Saving progress on Camera Controller
    Also made a .gitignore

--- sw/camera_controller.py
+++ sw/camera_controller.py
@@ -1,19 +1,22 @@
 
-
-from serial import Serial
+from time import sleep
+import serial
 import json
 
 if __name__== "__main__":
 
-    s = Serial("/dev/ttyUSB0", 115200)
-    s.open()
-
+    s = serial.Serial("/dev/ttyUSB0", 115200)
+    print(s.isOpen())
+    sleep(5)
+    print(s.write(b"b"))
+    s.flush()
     while True:
         # wait on serial communication
-        if s.in_waiting() > 0:
+        if s.in_waiting > 0:
             # take picture if you get a message
             print("take picture")
             # send a response
+            sleep(5)
             s.write(b"finished")
-            
-
+            sleep(5)
+            s.flush()
