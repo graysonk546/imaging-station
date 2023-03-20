2cc6fd977e398bc9e171402abba45dcf075a3d92
Grayson King <graysonk546@gmail.com>
2023-03-19 21:36:23 -0700

    added logic for 'finished-imaging' cmd and removed unecessary sleep calls

--- sw/camera_controller.py
+++ sw/camera_controller.py
@@ -14,65 +14,50 @@ FILE_NAME =   "/pic_{n}.tiff"
 
 def setup_camera(self, cam: Camera):
     with cam:
-        # Enable auto exposure time setting if camera supports it
+        # set the camera exposure
         try:
-            # cam.ExposureAuto.set('Continuous')
             cam.ExposureTime.set(8999999)
-
-            # Uncomment these commands to set a custom exposure, its range is around 10 to 1e7 us
-            # Can run script sw/python-test-scripts/Examples_from_Vimba/list_features.py
-            # to see all features on a camera and its values
-            # cam.ExposureTime.set(100)
-            # cam.ExposureAuto.set('Off')
-
         except (AttributeError, VimbaFeatureError):
             pass
-
-        # Enable white balancing if camera supports it
+        # set the camera white balance
         try:
             cam.BalanceWhiteAuto.set('Continuous')
-
         except (AttributeError, VimbaFeatureError):
             pass
-
-        # Try to adjust GeV packet size. This Feature is only available for GigE - Cameras.
+        # only for GigE cams, try removing this
         try:
             cam.GVSPAdjustPacketSize.run()
-
             while not cam.GVSPAdjustPacketSize.is_done():
                 pass
-
         except (AttributeError, VimbaFeatureError):
             pass
 
         # Query available, open_cv compatible pixel formats
         # prefer color formats over monochrome formats
-        cv_fmts = intersect_pixel_formats(cam.get_pixel_formats(), OPENCV_PIXEL_FORMATS)
+        cv_fmts = intersect_pixel_formats(cam.get_pixel_formats(), \
+                                          OPENCV_PIXEL_FORMATS)
         color_fmts = intersect_pixel_formats(cv_fmts, COLOR_PIXEL_FORMATS)
 
         if color_fmts:
             cam.set_pixel_format(color_fmts[0])
-
         else:
             mono_fmts = intersect_pixel_formats(cv_fmts, MONO_PIXEL_FORMATS)
-
             if mono_fmts:
                 cam.set_pixel_format(mono_fmts[0])
-
             else:
-                abort('Camera does not support a OpenCV compatible format natively. Abort.')
-
-
+                abort('Camera does not support a OpenCV compatible format')
 
 if __name__== "__main__":
     # establish serial communication with Bluepill
     s = serial.Serial("/dev/ttyUSB0", 115200)
-    print(s.isOpen())
-    sleep(5)
+    
+    # commence the imaging session with the "start" command
+    sleep(1)
     print(s.write(b"start\n"))
     s.flush()
-    # make a directory for the sample images
-    date = datetime.now().strftime("%d_%m_%Y|%H_%M_%S")
+
+    # make a directory to temporarily store the images
+    date = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
     f = FOLDER_NAME.format(date=date)
     os.mkdir(f)
     n = 0
@@ -80,21 +65,26 @@ if __name__== "__main__":
     while True:
         # wait on serial communication
         if s.in_waiting > 0:
-            sleep(1)
-            if s.readline().decode("ascii") == "picture\r\n":
-                print("here")
+            # sleep(1)
+            message = s.readline().decode("ascii")
+            if message == "picture\r\n":
+                # requirement that Vimba instance is opened using "with"
                 with Vimba.get_instance () as vimba:
                     cams = vimba.get_all_cameras ()
-                    # Have to write this "with statement" before trying to use a vimba camera
                     with cams [0] as cam:
+                        # set frame capture timeout at max exposure time
                         frame = cam.get_frame(timeout_ms=1000000)
                         print("Got a frame")
-                        time.sleep(1)
+                        # time.sleep(1)
+                        print("Frame saved to mem")
                         frame.convert_pixel_format(PixelFormat.Mono8)
-                        cv2.imwrite(f+FILE_NAME.format(n=n), frame.as_opencv_image ())
+                        cv2.imwrite(f+FILE_NAME.format(n=n), \
+                                    frame.as_opencv_image())
                         n+=1
-                        # indicate that a picture was taken
-                        print("take picture")
-                        # send a response
+                        # send a message to indicate a picture was saved
                         s.write(b"finished\n")
                         s.flush()
+                        
+            elif message == "finished-imaging\r\n":
+                # exit the control loop
+                break
